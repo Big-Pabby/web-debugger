f004895009005943d29059e83a19ae8c5dc40c81
Victor <victoradekunle312@gmail.com>
2022-07-05 11:56:23 +0000

    app page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" container">
+        <h2>Web Debugger</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
